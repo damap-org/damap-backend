9e7201292db05af5ad2482bfbc950b5a481a203f
Sotiris Tsepelakis <stsepelakis@gmail.com>
2026-03-11 17:34:34 +0100

    scripts: rename tls generator and fix paths

--- docker/prod/scripts/generate-tls-pair.sh
+++ docker/prod/scripts/generate-tls-pair.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-openssl req -x509 -out tls/bundle.crt -keyout tls/private.pem \
+openssl req -x509 -out docker/prod/tls/bundle.crt -keyout docker/prod/tls/private.pem \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=damap.localhost' -extensions EXT -config <( \
    printf "[dn]\nCN=damap.localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:damap.localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
