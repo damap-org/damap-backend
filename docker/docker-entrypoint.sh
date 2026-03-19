6457728c80e7e11fe3c5af3233a10fe1f286cde6
Sotiris Tsepelakis <stsepelakis@gmail.com>
2026-03-19 18:51:02 +0100

    Dev: prefix all DB vars with DAMAP_ & entrypoint enhancements

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,29 +7,32 @@
 set -euo pipefail
 
 log() {
-    # Usage: log "message" "LEVEL"
-    local msg="$1"
-    local level="${2:-INFO}"
-    echo "$(date +"%Y-%m-%d %H:%M:%S,%3N") $level  [docker-entrypoint.sh] $msg"
+	# Usage: log "message" "LEVEL"
+	local msg="$1"
+	local level="${2:-INFO}"
+	echo "$(date +"%Y-%m-%d %H:%M:%S,%3N") $level  [docker-entrypoint.sh] $msg"
 }
 
-# Default DB kind is PostgreSQL, unless overridden by QUARKUS_DATASOURCE_DB_KIND.
-: "${QUARKUS_DATASOURCE_DB_KIND:=postgresql}"
+# Require DB kind to be explicitly set.
+: "${QUARKUS_DATASOURCE_DB_KIND:?QUARKUS_DATASOURCE_DB_KIND must be set. Supported: oracle, postgresql.}"
 
-if [ "$QUARKUS_DATASOURCE_DB_KIND" = "postgresql" ]; then
-    log "Using datasource configuration for PostgreSQL."
-    export QUARKUS_DATASOURCE_JDBC_DRIVER="org.postgresql.Driver"
-    export QUARKUS_DATASOURCE_JDBC_URL="jdbc:postgresql://${DAMAP_DB_HOST:-postgres-db}:${DAMAP_DB_PORT:-5432}/${DAMAP_DB_NAME:-damap}"
-    export QUARKUS_HIBERNATE_ORM_DIALECT="org.hibernate.dialect.PostgreSQLDialect"
-elif [ "$QUARKUS_DATASOURCE_DB_KIND" = "oracle" ]; then
-    log "Using datasource configuration for Oracle."
-    export QUARKUS_DATASOURCE_JDBC_DRIVER="oracle.jdbc.driver.OracleDriver"
-    export QUARKUS_DATASOURCE_JDBC_URL="jdbc:oracle:thin:@${DAMAP_DB_HOST:-oracle-db}:${DAMAP_DB_PORT:-1521}/${DAMAP_DB_NAME:-FREEPDB1}"
-    export QUARKUS_HIBERNATE_ORM_DIALECT="org.hibernate.dialect.OracleDialect"
-else
-    log "Unknown QUARKUS_DATASOURCE_DB_KIND: $QUARKUS_DATASOURCE_DB_KIND. Supported: oracle, postgresql." "ERROR" 
-    exit 1
-fi
+case "$QUARKUS_DATASOURCE_DB_KIND" in
+postgresql)
+	log "Using database configuration for PostgreSQL."
+	export QUARKUS_DATASOURCE_JDBC_URL="jdbc:postgresql://${DAMAP_DB_HOST:-postgres-db}:${DAMAP_DB_PORT:-5432}/${DAMAP_DB_NAME:-damap}"
+	# driver and dialect already handled by application.yaml
+	;;
+oracle)
+	log "Using database configuration for Oracle."
+	export QUARKUS_DATASOURCE_JDBC_URL="jdbc:oracle:thin:@${DAMAP_DB_HOST:-oracle-db}:${DAMAP_DB_PORT:-1521}/${DAMAP_DB_NAME:-FREEPDB1}"
+	export QUARKUS_DATASOURCE_JDBC_DRIVER="oracle.jdbc.driver.OracleDriver"
+	export QUARKUS_HIBERNATE_ORM_DIALECT="org.hibernate.dialect.OracleDialect"
+	;;
+*)
+	log "Unknown QUARKUS_DATASOURCE_DB_KIND: $QUARKUS_DATASOURCE_DB_KIND. Supported: oracle, postgresql." "ERROR"
+	exit 1
+	;;
+esac
 
 log "Running Quarkus re-augmentation."
 java ${JAVA_OPTIONS} -Dquarkus.launch.rebuild=true -jar quarkus-run.jar
