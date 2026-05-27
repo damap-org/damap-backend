f83e015125885a003b23ae7824a068339612e65c
bitmotivator <213508235+bitmotivator@users.noreply.github.com>
2026-05-27 12:28:42 +0200

    Initial set of tests

---
+++ src/test/java/org/damap/base/integration/generic/GenericTestProfile.java
@@ -0,0 +1,20 @@
+package org.damap.base.integration.generic;
+
+import io.quarkus.test.junit.QuarkusTestProfile;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenericTestProfile implements QuarkusTestProfile {
+  @Override
+  public Map<String, String> getConfigOverrides() {
+    Map<String, String> overrides = new HashMap<>();
+
+    overrides.put("damap.generic-cris-endpoint-url", "http://127.0.0.1:8888");
+    overrides.put("damap.generic-cris-api-key", "test-api-key");
+    overrides.put(
+        "damap.person-services",
+        "[{\"display-text\": \"Test\", \"query-value\": \"TEST\", \"class-name\": \"org.damap.base.integration.generic.GenericPersonService\"}]'");
+
+    return overrides;
+  }
+}
